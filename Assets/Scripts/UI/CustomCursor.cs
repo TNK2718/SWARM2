f42f4b348f7aff5302b97cca7a8b87837aa8950c
yoshihiro-oguni <38971031+yoshihiro-oguni@users.noreply.github.com>
2019-03-09 17:45:05 +0900

    マウスカーソルの作成（UI/CustomCursor）

---
+++ Assets/Scripts/UI/CustomCursor.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI {
+    public class CustomCursor : MonoBehaviour {
+        public Texture2D image;
+        private GameObject cursorSprite;
+
+        void Start() {
+            if (image == null) {
+                return;
+            }
+
+            Cursor.visible = false;
+            cursorSprite = new GameObject("cursorSprite");
+            cursorSprite.AddComponent<SpriteRenderer>();
+            cursorSprite.GetComponent<SpriteRenderer>().sprite =
+                Sprite.Create(
+                    image,
+                    new Rect(0, 0, image.width, image.height),
+                    new Vector2(0.5f, 0.5f),
+                    100f);
+        }
+
+        void Update() {
+            if (image == null) {
+                return;
+            }
+
+            Cursor.visible = false;  // マウスが画面外に出るとvisibleがtrueになるため毎フレームセット
+            var newCursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newCursorPos.z = 10f;
+            cursorSprite.transform.position = newCursorPos;
+
+            // 回転させる
+            var angle = cursorSprite.transform.eulerAngles;
+            angle.z -= 7f;
+            cursorSprite.transform.eulerAngles = angle;
+        }
+    }
+}
